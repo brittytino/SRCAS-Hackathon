33f46514a8d6e16022c38a1eee5e7f9aba1dda1c
Tino Britty <153193545+brittytino@users.noreply.github.com>
2024-05-08 11:47:28 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,10 +4,10 @@ import "react-toastify/dist/ReactToastify.css";
 import "./clash_grotesk.css";
 
 export const metadata = {
-  title: "HackSphere - SRCAS Hackathon",
+  title: "HackByte - IIITDMJ Hackathon",
   description:
-    "HackSphere is the SRCAS's student-run hackathon, which centers on bringing developers and problem solvers from different foundations together and enables them to develop projects that can bring out an impact.",
-  keywords: "hackathon, SRCAS, hacksphere, coding, programming, tech",
+    "HackByte is the IIITDMJ's student-run hackathon, which centers on bringing developers and problem solvers from different foundations together and enables them to develop projects that can bring out an impact.",
+  keywords: "hackathon, IIITDMJ, hackbyte, coding, programming, tech",
   url: "https://hackbyte.in",
   openGraph: {
     title: "HackByte - IIITDMJ Hackathon",

8844bc32dbde821dbe30883ab6b8705a6ac86cd9
John Samuel J <121540092+johnsamuel05@users.noreply.github.com>
2024-06-12 22:18:47 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -185,7 +185,7 @@ export default function Home() {
           </div>
           <div className="flex flex-col items-start gap-3 w-full lg:items-end lg:w-1/2">
             <p
-              className="text-[#FAF8ED] text-[1.25rem] font-medium text-left 
+              className="text-[#FFF] text-[1.25rem] font-medium text-left 
               lg:text-right lg:text-[1.5rem]"
             >
               Join us at SRCAS, August 14

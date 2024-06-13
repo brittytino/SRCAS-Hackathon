f5f04f546c212eac83feb7f6258919cc03d53b61
John Samuel J <121540092+johnsamuel05@users.noreply.github.com>
2024-06-13 07:41:34 +0530

    Update page.jsx

--- app/prizes/page.jsx
+++ app/prizes/page.jsx
@@ -182,12 +182,12 @@ export default function Prizes() {
       >
         <div className="w-full flex flex-col gap-10 md:gap-16">
           <div className="flex flex-col items-center gap-4">
-            <p className="text-[#F5F0D8] text-[2.25rem] font-normal md:text-[5rem]">
+            <p className="text-[#FFF] text-[2.25rem] font-normal md:text-[5rem]">
               Prizes
             </p>
             <div className="flex flex-col items-center gap-3 md:gap-4">
               <p
-                className="w-full lg:max-w-[80%] text-[#C3C3C3] font-[Inter] 
+                className="w-full lg:max-w-[100%] text-[#FFF] font-[Inter] 
                   text-center font-normal text-[1.25rem] md:text-[1.5rem]"
               >
                 Combine your skills and creativity to build something outstanding!

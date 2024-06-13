e9f2b75f5da3816d9f3a5e9fda6878b818e05339
thisisthesarath <149189766+thisisthesarath@users.noreply.github.com>
2024-06-13 09:22:09 +0530

    Update page.jsx

--- app/humans/page.jsx
+++ app/humans/page.jsx
@@ -31,7 +31,6 @@ import aanchal from "@/public/humansPage/aanchal.jpeg";
 import HODimg from "@/public/Co-Ordinators/sumathi.png";
 import AssociateProf from "@/public/Co-Ordinators/praneesh.png";
 import AssociateProf2 from "@/public/Co-Ordinators/manoj.png";
-import Principal from "@/public/Co-ordinators/principalsrcas.jpeg";
 
 import FooterAnimation from "@/components/FooterAnimation";
 import Footer from "@/components/Footer";
@@ -59,7 +58,7 @@ export default function Team() {
   const organizerData = [
     {
       name: "Tino Britty J",
-      role: "Organizer",
+      role: "Chairman",
       profilepic: chairman,
       linkedin: "https://www.linkedin.com/in/brittytino",
       instagram: "https://www.instagram.com/brittytino/",
@@ -67,7 +66,7 @@ export default function Team() {
     },    
     {
       name: "John Samuel",
-      role: "Organizer",
+      role: "Vice Chairman",
       profilepic: assistantChairman,
       linkedin: "https://www.linkedin.com/in/samjohnoffl",
       instagram: "https://www.instagram.com/samjohn_offl",
@@ -75,7 +74,7 @@ export default function Team() {
     },
     {
       name: "Pravin B",
-      role: "Organizer",
+      role: "Secretary",
       profilepic: secretary,
       linkedin: "https://www.linkedin.com/in/pravinboopathi/",
       instagram: "https://www.instagram.com/bpravin._/",
@@ -83,7 +82,7 @@ export default function Team() {
     },
     {
       name: "Kanishka C",
-      role: "Organizer",
+      role: "Joint Secretary",
       profilepic: viceChairman,
       linkedin: "https://www.linkedin.com/in/kaniskha-c-2124a5292/",
       instagram: "https://www.instagram.com/kaniskha_chandrasekar/",
@@ -91,7 +90,7 @@ export default function Team() {
     },
     {
       name: "Sarath P",
-      role: "Web Dev",
+      role: "Executive Member",
       profilepic: Sarath,
       linkedin: "http://www.linkedin.com/in/thisisthesarath",
       instagram: "http://www.instagram.com/sarath.online",
@@ -99,7 +98,7 @@ export default function Team() {
     },
     {
       name: "Aarthi S",
-      role: "Organizer",
+      role: "Member",
       profilepic: Aarthi,
       linkedin: "https://www.linkedin.com/in/aarthi-s-8914a9292?",
       instagram: "https://www.instagram.com/__.aarthi___?",
@@ -107,14 +106,14 @@ export default function Team() {
     },
     {
       name: "Balaguru K",
-      role: "Content Creator",
+      role: "Member",
       profilepic: Balaguru,
       linkedin: "https://www.linkedin.com/in/balaguru-k-22157727a?",
       github: "https://www.github.com"
     },
     {
       name: "Chittesh",
-      role: "Designer",
+      role: "Member",
       profilepic: Chittesh,
       linkedin: "https://www.linkedin.com/in/chittesh-undefined-753499306?",
       instagram: "https://www.instagram.com/chittesh___23?",
@@ -122,7 +121,7 @@ export default function Team() {
     },
     {
       name: "Gokul S",
-      role: "Media",
+      role: "Member",
       profilepic: Gokul,
       linkedin: "https://www.linkedin.com/in/gokul-saravanan-s-74bb9327b?",
       instagram: "https://www.instagram.com/lil_dodge5?",
@@ -130,7 +129,7 @@ export default function Team() {
     },
     {
       name: "Harini",
-      role: "Content Creator",
+      role: "Member",
       profilepic: harini,
       linkedin: "https://www.linkedin.com/in/harini-pechimuthu-1b47972b5",
       instagram: "https://www.instagram.com/harinipechimuthu?",
@@ -138,7 +137,7 @@ export default function Team() {
     },
     {
       name: "Logeshwaran V",
-      role: "Designer",
+      role: "Member",
       profilepic: logeshwaran,
       linkedin: "https://www.linkedin.com/in/logesh-waran-a42395227?",
       instagram: "https://www.instagram.com/aeksio__hen__vys?",
@@ -146,21 +145,21 @@ export default function Team() {
     },
     {
       name: "Nandita",
-      role: "Volunteer",
+      role: "Member",
       profilepic: Nandita,
       linkedin: "https://www.linkedin.com/in/nandita-s-a38948312?",
       github: "https://www.github.com"
     },
     {
       name: "Priyadharshini K",
-      role: "Volunteer",
+      role: "Member",
       profilepic: PriyaDharshini,
       linkedin: "https://www.linkedin.com/in/priyadharshini-k-784920275",
       github: "https://www.github.com"
     },
     {
       name: "Poornila I P",
-      role: "Volunteer",
+      role: "Member",
       profilepic: Poornila,
       linkedin: "https://www.linkedin.com/in/poornila-i-p-45b0b4292?",
       instagram: "https://www.instagram.com/click_.natural?",
@@ -168,21 +167,21 @@ export default function Team() {
     },
     {
       name: "Rishaba R U",
-      role: "Social Media Manager",
+      role: "Member",
       profilepic: Rishaba,
       linkedin: "https://www.linkedin.com/in/rishaba0305204?",
       github: "https://www.github.com"
     },
     {
       name: "Saratthi",
-      role: "Media",
+      role: "Member",
       profilepic: SriSaratthi,
       linkedin: "https://www.linkedin.com/in/saratthi-080555166",
       github: "https://www.github.com"
     },
     {
       name: "Srikanth R G",
-      role: "Web Dev",
+      role: "Member",
       profilepic: srikanth,
       linkedin: "https://www.linkedin.com/in/srikanth-r-g-544937283?",
       instagram: "https://www.instagram.com/spetsnaz_srikanth?",
@@ -190,14 +189,14 @@ export default function Team() {
     },
     {
       name: "Sujithaa J",
-      role: "Content Creator",
+      role: "Member",
       profilepic: Sujithaa,
       linkedin: "https://www.linkedin.com/in/sujithaa-j-41643b292?",
       github: "https://www.github.com"
     },
     {
       name: "Velan J",
-      role: "UI UX",
+      role: "Member",
       profilepic: Velan,
       linkedin: "https://www.linkedin.com/in/velan-jayashankar",
       instagram: "https://www.instagram.com/ig__velan__",
@@ -205,7 +204,7 @@ export default function Team() {
     },
     {
       name: "Vikaas K S",
-      role: "Organizer",
+      role: "Member",
       profilepic: Vikaas,
       linkedin: "https://www.linkedin.com/in/vikaas-k-s-7644a9292?",
       instagram: "https://www.instagram.com/vikaas_0304?",
@@ -213,7 +212,7 @@ export default function Team() {
     },
     {
       name: "Vishnuraj R",
-      role: "Media",
+      role: "Member",
       profilepic: Vishnuraj,
       linkedin: "https://www.linkedin.com/in/vishnuraj-r-a3229324b/",
       instagram: "https://www.instagram.com/vishnu._.rvr/",
@@ -231,16 +230,7 @@ export default function Team() {
 
   const mentorData = [
     {
-      name: "Dr.B.L.Shivakumar",
-      role: "Chief Patron",
-      profilepic: Principal,
-      linkedin: "https://www.linkedin.com/in/",
-      instagram: "https://instagram.com/",
-      github: "https://github.com/",
-    },
-    {
       name: "Dr.N.Sumathi",
-      role:"Mentor",
       profilepic: HODimg,
       linkedin: "https://www.linkedin.com/in/",
       instagram: "https://instagram.com/",
@@ -248,7 +238,6 @@ export default function Team() {
     },
     {
       name: "Dr.M.Praneesh",
-      role:"Mentor",
       profilepic: AssociateProf,
       linkedin: "https://www.linkedin.com/in/",
       instagram: "https://instagram.com/",
@@ -256,7 +245,6 @@ export default function Team() {
     },
     {
       name: "Mr.S.Manoj",
-      role:"Mentor",
       profilepic: AssociateProf2,
       linkedin: "https://www.linkedin.com/in/",
       instagram: "https://instagram.com/",
@@ -355,14 +343,14 @@ export default function Team() {
                 border border-[#D1CAC7] px-4 py-1 mb-3"
             >
               <p
-                className="w-full text-[#FFFFFF] font-[Inter] 
+                className="w-full text-[#C3C3C3] font-[Inter] 
                   text-center font-normal text-[1.25rem] tracking-[0.0625rem]"
               >
                 THE TEAM
               </p>
             </div>
             <p
-              className="text-[#FFFFFF] text-center text-[2.5rem] font-normal 
+              className="text-[#F5F0D8] text-center text-[2.5rem] font-normal 
                 md:text-[4rem] xl:text-[5rem] md:leading-[4.5rem] xl:leading-[5.25rem]"
             >
               Meet the humans of
